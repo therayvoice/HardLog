0452d521ce06d8836caed5e5d9236482e9a51c7f
ray <rayvanet@gmail.com>
2021-06-11 01:58:03 +0500

    removed test.js to avoide bloat

--- test.js
+++
@@ -1,6 +0,0 @@
-var hardlog = require('./hardlog.js');
-hardlog.init("Ray Voice", "1.0.3", "LogFile.txt");
-
-hardlog.write("First HardLog");
-hardlog.shadow("Write anything to hardlog here, without Terminal dispay!");
-
